53b0b994a4d09a813d4754932dbf024798bab325
David Ribeiro <davidflribeiro87@gmail.com>
2020-01-28 22:45:39 -0100

    first commit

---
+++ test/folders-endpoints.spec.js
@@ -0,0 +1,36 @@
+const knex = require('knex');
+const app = require('../src/app');
+const { makeFoldersArray } = require('./folders.fixtures');
+const { makeNotesArray } = require('./notes.fixtures');
+
+describe('Folders Endpoints', function() {
+  let db;
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL
+    });
+    app.set('db', db);
+  });
+
+  after('disconnect from db', () => db.destroy());
+
+  before('clean the table', () =>
+    db.raw('TRUNCATE noteful_folders, noteful_notes RESTART IDENTITY CASCADE')
+  );
+
+  afterEach('cleanup', () =>
+    db.raw('TRUNCATE noteful_folders, noteful_notes RESTART IDENTITY CASCADE')
+  );
+
+  describe.only(`GET /api/folders`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app)
+          .get('/api/folders')
+          .expect(200, []);
+      });
+    });
+  });
+});
---
+++ test/notes.fixtures.js
@@ -0,0 +1,22 @@
+function makeNotesArray() {
+  return [
+    {
+      id: 1,
+      name: 'test',
+      modified: '2019-01-03T00:00:00.000Z',
+      folder_id: '1',
+      content: 'olaaa'
+    },
+    {
+      id: 2,
+      name: 'test',
+      modified: '2019-01-03T00:00:00.000Z',
+      folder_id: '2',
+      content: 'ruaaaaaaaaaaaaaa'
+    }
+  ];
+}
+
+module.exports = {
+  makeNotesArray
+};
